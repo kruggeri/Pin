da0006cacc617928bee7d3e849ea3b68510fec54
Kate Ruggeri <k.ruggeri8@gmail.com>
2017-06-20 10:23:41 -0400

    Create users migration

---
+++ db/migrate/20170620134156_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :firstname 
+      t.string :lastname
+      t.string :email
+      t.text :about
+      t.string :location
+      t.string :personal_site_url
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+    end
+  end
+end
---
+++ docs/api-endpoints.md
@@ -0,0 +1,16 @@
+API Endpoints
+
+HTML API
+
+- Root
+    - GET /  (loads React web App)
+
+JSON API
+
+- Users
+    - POST /api/users
+    - PATCH /api/users
+
+- Session
+    - POST /api/session
+    - DELETE /api/session
